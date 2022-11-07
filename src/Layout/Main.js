32b8acd0eedfe2de98f19cdaeda856bd231cacd2
ikhlas5 <mohammadikhlas765@gmail.com>
2022-11-08 00:36:00 +0600

    third commit

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Footer/Footer';
 import Header from '../Shared/Header/Header';
 
 const Main = () => {
@@ -7,7 +8,7 @@ const Main = () => {
         <div>
             <Header></Header>
             <Outlet></Outlet>
-            
+            <Footer></Footer>
         </div>
     );
 };
